52fd3d3ddbb9700c77edd3fbe7913f523f43d3ce
Devon Gillis <devongillis88@gmail.com>
2022-07-25 19:56:02 -0300

    utting the finishing touches on the css. got some javascript left to write and media queries

--- js/app.js
+++ js/app.js
@@ -1,38 +1,15 @@
-class ChartData {
-    labels = [];
-    datasets = [];
-    options = {};
+const primaryColor = '#7477bf';
 
-    /**
-     * Constructs a chart data class to populate charts with
-     * 
-     * @param {array} labels - the arr 
-     * @param {*} sentData 
-     * @param {*} sentLabel 
-     * @param {*} legend 
-     */
-    constructor (labels, sentData, sentLabel, legend) {
-        this.labels = labels;
-        this.datasets = [{
-            label: sentLabel,
-            data: sentData,
-        }];
-        this.options = {
-
-        }
-    }
-}
-
-const trafficHourlyData = new ChartData(['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'], [700, 1250, 1150, 1950, 1500, 1650, 1050, 1900, 2300, 1500, 2500],);
-const trafficDailyData = new ChartData(['S', 'M', 'T', 'W', 'T', 'F', 'S'], [65, 115, 165, 80, 230, 200, 100],);
-const trafficWeeklyData = new ChartData(['week 1', 'week 2', 'week 3', 'week 4', 'week 5', 'week 6'], [3500, 4000, 3600, 4300, 3200, 3600]);
-const trafficMonthlyData = new ChartData(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], [15000, 21000, 17600, 18900, 21000, 25400, 18300, 15000, 16700, 24500, 21400, 19500],);
-// const dailyTrafficBarData = new ChartData(['S', 'M', 'T', 'W', 'T', 'F', 'S'],[65, 115, 165, 80, 230, 200, 100],);
-// const mobileData = new ChartData(['Desktop', 'Tablet', 'Phones'], [700, 245, 357],);
+const trafficHourlyData = generateChartDataObject(['16-22', '23-29', '30-5', '6-12', '13-19', '20-26', '27-3', '4-10', '11-17', '18-24', '25-31'], [700, 1250, 1150, 1950, 1500, 1650, 1050, 1900, 2300, 1500, 2500], 'line');
+const trafficDailyData = generateChartDataObject(['S', 'M', 'T', 'W', 'T', 'F', 'S'], [65, 115, 165, 80, 230, 200, 100], 'line');
+const trafficWeeklyData = generateChartDataObject(['week 1', 'week 2', 'week 3', 'week 4', 'week 5', 'week 6'], [3500, 4000, 3600, 4300, 3200, 3600], 'line');
+const trafficMonthlyData = generateChartDataObject(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'], [15000, 21000, 17600, 18900, 21000, 25400, 18300, 15000, 16700, 24500, 21400, 19500], 'line');
+const dailyTrafficBarData = generateChartDataObject(['S', 'M', 'T', 'W', 'T', 'F', 'S'],[65, 115, 165, 80, 230, 200, 100], 'bar');
+const mobileData = generateChartDataObject(['Desktop', 'Tablet', 'Phones'], [700, 245, 357], 'doughnut');
 
 const trafficChart = buildChart('line', '#traffic-chart', trafficHourlyData);
-const dailyChart = buildChart('bar', '#daily-traffic', new ChartData(['S', 'M', 'T', 'W', 'T', 'F', 'S'],[65, 115, 165, 80, 230, 200, 100],));
-const mobileChart = buildChart('doughnut', '#mobile-users', new ChartData(['Desktop', 'Tablet', 'Phones'], [700, 245, 357],));
+const dailyChart = buildChart('bar', '#daily-traffic', dailyTrafficBarData);
+const mobileChart = buildChart('doughnut', '#mobile-users', mobileData);
 
 const messageForm = document.querySelector('.message form');
 
@@ -49,7 +26,53 @@ function buildChart(type, ctx, sentData) {
     return new Chart(document.querySelector(`${ctx}`), {
         type: `${type}`,
         data: sentData,
+        options: generateChartOptions(type),
     })
+}
+
+function generateChartOptions(type) {
+    let options = {
+        plugins:{
+            legend:{
+                labels: {
+
+                }
+            },
+        },
+    }
+    if (type === 'bar' || type === 'line') {
+        options.plugins.legend.display = false;
+    } else if (type === 'doughnut'){
+        options.plugins.legend.position = 'right';
+        options.plugins.legend.labels.boxWidth = 25;
+        options.plugins.legend.labels.boxHeight = 15;
+    }
+    return options
+}
+
+function generateChartDataObject(labels, data, type){
+    const chartData = {
+        labels: labels,
+        datasets: [{
+            data: data,
+        }]
+    }
+    if (type === 'line'){
+        chartData.datasets[0].borderColor = primaryColor;
+        chartData.datasets[0].pointBorderWidth = 5;
+        chartData.datasets[0].backgroundColor = 'rgba(116, 119, 191, 50%)';
+    }
+    else if(type === 'bar') {
+        chartData.datasets[0].backgroundColor = primaryColor;
+    }
+    else if(type ==='doughnut'){
+        chartData.datasets[0].backgroundColor = [
+            primaryColor,
+            '#81c98f',
+            '#51b6c8',
+        ];
+    }
+    return chartData;
 }
 
 messageForm.addEventListener('submit', (e) => {
